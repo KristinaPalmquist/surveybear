1d706b35cc2953761a0a1a252f1a13e83715cc96
Kristina_Palmquist <kristina.palmquist@gmail.com>
2023-06-09 10:30:18 +0200

    added port

--- apps/surveybear-api/src/main.ts
+++ apps/surveybear-api/src/main.ts
@@ -2,6 +2,7 @@ import {connect} from "@surveybear/surveybear-lib"
 import app from "./app";
 
 const port = process.env.port || 3333;
+// const port = process.env.port || 3333;
 
 try {
   connect().then(() => {
